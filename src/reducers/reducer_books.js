d4a1afecf8baced6222987f757c7069c3af4e3e3
paderinandrey <paderinandrey2011@gmail.com>
2017-01-02 20:44:41 +0300

    Reducers and actions

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' }
-  ]
+    { title: 'Javascript: The Good Parts', pages: 101, genre: 'classic' },
+    { title: 'Harry Potter', pages: 39, genre: 'classic' },
+    { title: 'The Dark Tower', pages: 85, genre: 'classic' },
+    { title: 'Eloquent Ruby', pages: 1, genre: 'classic' }
+  ];
 }
